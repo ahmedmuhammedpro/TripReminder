8ef004569d2a5ad9d5b7703b60fb4efda2888525
ahmedpro <ahmedmuhammedpro@gmail.com>
2020-02-17 17:54:57 +0200

    first commit

---
+++ app/src/main/java/com/example/tripreminder/view/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tripreminder.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tripreminder.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
